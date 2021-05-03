fae7a1c1413c67b6574a345f261c037f4fc13bae
wadoodaref <wadoodaref@gmail.com>
2021-05-03 18:07:55 +0300

    initial author routes

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-if(process.env.NODE_ENV !== 'production'){
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
 }
 const express = require('express')
-const app=express()
-const expressLayouts=require('express-ejs-layouts')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter=require('./routes/index')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname+'/views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({limit:'10mb', extended:false}))
 
 const mongoose = require('mongoose')
 // const { on } = require('nodemon')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db=mongoose.connection
-db.on('error',(error)=>{console.error(error)})
+const db = mongoose.connection
+db.on('error', (error) => { console.error(error) })
 db.once('open', () => { console.log('connected to mongoose') })
 
 
 
 app.use('/', indexRouter)
-
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
